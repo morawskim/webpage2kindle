5ba44258bab4d26baa0df83a357deff511bfb039
Marcin Morawski <marcin@morawskim.pl>
2022-08-15 07:22:25 +0200

    Install monolog and sentry bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
