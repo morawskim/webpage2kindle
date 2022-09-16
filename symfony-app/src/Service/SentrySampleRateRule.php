e3c75a5696c78d65dd9cc9b69626e7415ffc8e73
Marcin Morawski <marcin@morawskim.pl>
2022-09-16 08:18:32 +0200

    [symfony-app] Set value of sample_rate based on route name
    This change decrease number of transactions in Sentry, for endpoint with prometheus'  metrics.

---
+++ symfony-app/src/Service/SentrySampleRateRule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use Sentry\Tracing\SamplingContext;
+
+class SentrySampleRateRule
+{
+    public function __invoke(SamplingContext $context): float
+    {
+        if ($context->getParentSampled()) {
+            // If the parent transaction (for example a JavaScript front-end)
+            // is sampled, also sample the current transaction
+            return 1.0;
+        }
+
+        if ('prometheus_metrics' === ($context->getTransactionContext()?->getTags()['route'] ?? '')) {
+            return 0.1;
+        }
+
+        // Default sample rate for all other transactions (replaces `traces_sample_rate`)
+        return 1.0;
+    }
+}
