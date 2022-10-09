97aa8beb535140278328d524586bcba416242002
Marcin Morawski <marcin@morawskim.pl>
2022-10-09 09:43:20 +0200

    [node-microservice] Integrate with Sentry

--- node-readability/microservice.ts
+++ node-readability/microservice.ts
@@ -1,11 +1,28 @@
 import express, { Express, Request, Response } from 'express';
 import { Readability } from '@mozilla/readability';
 import { JSDOM } from 'jsdom';
+import * as dotenv from 'dotenv';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
+dotenv.config({ path: "/app/.env" });
 const app: Express = express();
 const port = 3000;
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
 
+app.use(Sentry.Handlers.requestHandler({transaction: "methodPath"}));
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+
 app.post('/process-webpage', (req: Request, res: Response) => {
     const {body, url} = req.body;
     const doc = new JSDOM(body, {
@@ -23,6 +40,9 @@ app.post('/process-webpage', (req: Request, res: Response) => {
     res.json(responseObject);
 });
 
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
 app.listen(port, () => {
-    console.log(`Microservice is listening on port ${port}`)
+    console.log(`Microservice is listening on port ${port}`);
 });
