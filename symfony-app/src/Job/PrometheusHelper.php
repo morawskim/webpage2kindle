0e1c5e49a08dde8618006ca8b54165e27b342fb6
Marcin Morawski <marcin@morawskim.pl>
2022-11-11 09:58:50 +0100

    [symfony-app] Publish request metrics (time and memory) to prometheus

--- symfony-app/src/Job/PrometheusHelper.php
+++ symfony-app/src/Job/PrometheusHelper.php
@@ -28,4 +28,25 @@ class PrometheusHelper
             ['adapter']
         )->incBy($bytes, [$adapter]);
     }
+
+    public function publishRequestMetrics(array $data, float $duration, int $memoryPeakBytes): void
+    {
+        $histogram = $this->collectorRegistry->getOrRegisterHistogram(
+            $this->metricNamespace,
+            'execution_time',
+            'Execution time (ms)',
+            array_keys($data),
+            [50, 150, 250, 400, 500, 700, 900, 1000, 1200, 1500, 2500, 4000]
+        );
+        $histogram->observe($duration, array_values($data));
+
+        $histogram = $this->collectorRegistry->getOrRegisterHistogram(
+            $this->metricNamespace,
+            'memory_usage',
+            'Memory usage (MiB)',
+            array_keys($data),
+            [10, 25, 40, 50, 75, 90, 128, 150, 180, 200, 256]
+        );
+        $histogram->observe((int)($memoryPeakBytes / 1024 / 1024), array_values($data));
+    }
 }
---
+++ symfony-app/src/Web/Infrastructure/Symfony/EventSubscriber/PerformanceSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Web\Infrastructure\Symfony\EventSubscriber;
+
+use App\Job\PrometheusHelper;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class PerformanceSubscriber implements EventSubscriberInterface
+{
+    private array $data;
+
+    public function __construct(private readonly PrometheusHelper $prometheusHelper)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            TerminateEvent::class => ['onKernelTerminate', -1024],
+            RequestEvent::class => ['onKernelRequest', -1024],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $this->data['route'] = $event->getRequest()->get('_route') ?? 'unknown-route';
+        $this->data['method'] = $event->getRequest()->getRealMethod();
+    }
+
+    public function onKernelTerminate(TerminateEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $data = $this->data;
+        $data['status_code'] = $event->getResponse()->getStatusCode();
+        $duration = ceil((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 1000);
+        $memoryPeak = memory_get_peak_usage(true);
+
+        $this->prometheusHelper->publishRequestMetrics($data, $duration, $memoryPeak);
+    }
+}
