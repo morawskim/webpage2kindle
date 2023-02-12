d10f198261d1d6fcd3a1528727c05503a6ae7eda
Marcin Morawski <marcin@morawskim.pl>
2023-02-12 10:20:25 +0100

    [symfony-app] Add listener to Exception event
    Sending request to not existing route, caused malformed metrics for Prometheus. Those metric didn't have all required labels (missing route and method)

--- symfony-app/src/Web/Infrastructure/Symfony/EventSubscriber/PerformanceSubscriber.php
+++ symfony-app/src/Web/Infrastructure/Symfony/EventSubscriber/PerformanceSubscriber.php
@@ -4,6 +4,7 @@ namespace App\Web\Infrastructure\Symfony\EventSubscriber;
 
 use App\Job\PrometheusHelper;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
@@ -20,10 +21,21 @@ class PerformanceSubscriber implements EventSubscriberInterface
         return [
             TerminateEvent::class => ['onKernelTerminate', -1024],
             RequestEvent::class => ['onKernelRequest', -1024],
+            ExceptionEvent::class => ['onKernelException', 0],
         ];
     }
 
     public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $this->data['route'] = $event->getRequest()->get('_route') ?? 'unknown-route';
+        $this->data['method'] = $event->getRequest()->getRealMethod();
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
     {
         if (!$event->isMainRequest()) {
             return;
