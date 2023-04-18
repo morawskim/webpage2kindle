e19f9e9e66c1111839f13c7c6bd4d878555b1359
Marcin Morawski <marcin@morawskim.pl>
2023-04-18 16:47:15 +0200

    [symfony-app] Integrate w go-readability-api

---
+++ symfony-app/src/Job/PageContentFetcher/GoReadabilityApiPageContentFetcher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Job\PageContentFetcher;
+
+use App\Job\Exception\CannotGetPageContentException;
+use Symfony\Component\HttpClient\HttpOptions;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GoReadabilityApiPageContentFetcher implements PageContentFetcherInterface
+{
+    public function __construct(private readonly HttpClientInterface $goReadabilityMicroserviceClient)
+    {
+    }
+
+    public function getPageContent(string $url): string
+    {
+        $options = (new HttpOptions())
+            ->setQuery(['url' => $url])
+            ->toArray();
+        try {
+            $body = $this->goReadabilityMicroserviceClient->request('GET', '/', $options)->toArray();
+
+            return '<h1>' . $body['title'] . '</h1>' . $body['content'];
+        } catch (ServerExceptionInterface $e) {
+            throw CannotGetPageContentException::default('The page is not readable', $e);
+        }
+    }
+}
