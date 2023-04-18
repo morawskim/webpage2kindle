02532fff2e2bbcb75656a92ab77e72f5f17ce70d
Marcin Morawski <marcin@morawskim.pl>
2023-04-18 15:43:39 +0200

    [go-readability-api] Init project

---
+++ go-readability-api/go.mod
@@ -0,0 +1,20 @@
+module go-readability-api
+
+go 1.19
+
+require (
+	github.com/go-shiori/go-readability v0.0.0-20220215145315-dd6828d2f09b
+	github.com/spf13/cobra v1.7.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/go-shiori/dom v0.0.0-20210627111528-4e4722cd0d65 // indirect
+	github.com/gogs/chardet v0.0.0-20191104214054-4b6791f73a28 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ go-readability-api/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	nurl "net/url"
+	"strings"
+
+	readability "github.com/go-shiori/go-readability"
+	"github.com/spf13/cobra"
+)
+
+const index = `<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>go-readability</title>
+ </head>
+ <body>
+ <form action="/" style="width:80%">
+  <fieldset>
+   <legend>Get readability content</legend>
+   <p><label for="url">URL </label><input type="url" name="url" style="width:90%"></p>
+   <p><input type="checkbox" name="metadata" value="true">only get the page's metadata</p>
+  </fieldset>
+  <p><input type="submit"></p>
+ </form>
+ </body>
+</html>`
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "go-readability [flags] [source]",
+		Run:   rootCmdHandler,
+		Short: "go-readability is parser to fetch readable content of a web page",
+		Long: "go-readability is parser to fetch the readable content of a web page.\n" +
+			"The source can be an url or an existing file in your storage.",
+	}
+
+	rootCmd.Flags().StringP("http", "l", "", "start the http server at the specified address")
+	errFlag := rootCmd.MarkFlagRequired("http")
+
+	if errFlag != nil {
+		panic(errFlag)
+	}
+
+	err := rootCmd.Execute()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func rootCmdHandler(cmd *cobra.Command, args []string) {
+	// Start HTTP server
+	httpListen, _ := cmd.Flags().GetString("http")
+	if httpListen != "" {
+		http.HandleFunc("/", httpHandler)
+		log.Println("Starting HTTP server at", httpListen)
+		log.Fatal(http.ListenAndServe(httpListen, nil))
+	}
+
+	if len(args) == 0 {
+		cmd.Help()
+	}
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+	if url == "" {
+		w.Write([]byte(index))
+	} else {
+		log.Println("process URL", url)
+		content, err := getContent(url)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(content))
+	}
+}
+
+func getContent(srcPath string) (string, error) {
+	var (
+		pageURL   *nurl.URL
+		srcReader io.Reader
+	)
+
+	url, isURL := validateURL(srcPath)
+
+	if !isURL {
+		return "", fmt.Errorf("url %s does not look like valid url", url)
+	}
+
+	resp, err := http.Get(srcPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch web page: %v", err)
+	}
+	defer resp.Body.Close()
+
+	pageURL = url
+	srcReader = resp.Body
+
+	// Use tee so the reader can be used twice
+	buf := bytes.NewBuffer(nil)
+	tee := io.TeeReader(srcReader, buf)
+
+	// Make sure the page is readable
+	if !readability.Check(tee) {
+		return "", fmt.Errorf("failed to parse page: the page is not readable")
+	}
+
+	// Get readable content from the reader
+	article, err := readability.FromReader(buf, pageURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse page: %v", err)
+	}
+
+	metadata := map[string]interface{}{
+		"title":   article.Title,
+		"content": article.Content,
+	}
+
+	prettyJSON, err := json.MarshalIndent(&metadata, "", "    ")
+	if err != nil {
+		return "", fmt.Errorf("failed to write metadata file: %v", err)
+	}
+
+	return string(prettyJSON), nil
+}
+
+func validateURL(path string) (*nurl.URL, bool) {
+	url, err := nurl.ParseRequestURI(path)
+	return url, err == nil && strings.HasPrefix(url.Scheme, "http")
+}
