a2e4de66c10c274ba27bcb95a1e8dd8528b2b19d
Marcin Morawski <marcin@morawskim.pl>
2023-07-29 09:43:01 +0200

    [symfony-app] Add RateLimiter

---
+++ symfony-app/src/Infrastructure/Symfony/Event/RateLimiterSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Infrastructure\Symfony\Event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
+
+class RateLimiterSubscriber implements EventSubscriberInterface
+{
+    private RateLimiterFactory $apiLimiter;
+
+    public function __construct(RateLimiterFactory $apiLimiter)
+    {
+        $this->apiLimiter = $apiLimiter;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RequestEvent::class => ['onKernelRequest', 1000]
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $limiter = $this->apiLimiter->create($event->getRequest()->getClientIp());
+        $limiter->consume()->ensureAccepted();
+    }
+}
