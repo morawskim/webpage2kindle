a1a4332731ecd774d37ea667e356b6c41807249b
Marcin Morawski <marcin@morawskim.pl>
2023-09-03 09:33:48 +0200

    [symfony-app] Install request-id bundle

--- symfony-app/config/bundles.php
+++ symfony-app/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Nelexa\RequestDtoBundle\RequestDtoBundle::class => ['all' => true],
+    Chrisguitarguy\RequestId\ChrisguitarguyRequestIdBundle::class => ['all' => true],
 ];
