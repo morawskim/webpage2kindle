3e013f47e6c6ee9718d6efb50ce53bf7c238540c
Marcin Morawski <marcin@morawskim.pl>
2023-09-09 11:12:30 +0200

    [symfony-app] Catch exception when page is not readable (when using go microservice)

--- symfony-app/src/Job/PageContentFetcher/GoReadabilityApiPageContentFetcher.php
+++ symfony-app/src/Job/PageContentFetcher/GoReadabilityApiPageContentFetcher.php
@@ -3,6 +3,7 @@
 namespace App\Job\PageContentFetcher;
 
 use App\Job\Exception\CannotGetPageContentException;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\HttpOptions;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -24,6 +25,11 @@ class GoReadabilityApiPageContentFetcher implements PageContentFetcherInterface
             return '<h1>' . $body['title'] . '</h1>' . $body['content'];
         } catch (ServerExceptionInterface $e) {
             throw CannotGetPageContentException::default('The page is not readable', $e);
+        } catch (ClientException $e) {
+            if (400 === $e->getCode()) {
+                $msg = $e->getResponse()->getContent(false);
+                throw CannotGetPageContentException::default($msg, $e);
+            }
         }
     }
 }
